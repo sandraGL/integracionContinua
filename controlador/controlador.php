694e4bca099d343740198706088a36261685077e
adrians <adrian95sr@gmail.com>
2016-04-22 12:13:01 +0200

    Index.php terminado

---
+++ controlador/controlador.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<form method="get" action="controlador/Controlador.php">
+Saludo <input type="text" name="saludo"/><br/>
+<input type="submit" value="Enviar saludo"/>
+</form>
+</body>
+</html>
