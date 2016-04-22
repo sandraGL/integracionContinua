3bb7262979068f81df0656a03577ddb867c7e3b8
adrians <adrian95sr@gmail.com>
2016-04-22 12:19:54 +0200

    Plantilla terminada

---
+++ vista/plantillaSaludo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Plantilla Saludo</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h1>Este es el saludo</h1>
+
+		<p style="color:green">
+			<? echo $saludo;?>
+		</p>
+	</body>
+</html>
